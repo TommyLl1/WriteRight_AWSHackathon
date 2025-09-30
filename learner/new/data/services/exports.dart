b2bc15cea1eb3d9f4ee180f05d1b9d5be9116edf
TommyLl1 <tommyliu219@gmail.com>
2025-09-30 21:44:03 +0800

    Delete learner/new directory

--- learner/new/data/services/exports.dart
+++
@@ -1,8 +0,0 @@
-export 'auth_service.dart';
-export 'game_service.dart';
-export 'image_cache.dart';
-export 'task.dart';
-export 'user.dart';
-export 'wrong_character.dart';
-export 'permission.dart';
-export 'setting_service.dart';
